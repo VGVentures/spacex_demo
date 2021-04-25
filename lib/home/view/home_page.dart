7756ac3d785d7c85b159e8b98863cd9e881c7db9
Jeroen Meijer (Jay) <jeroenfkmeijer@gmail.com>
2021-04-25 22:14:51 +0200

    feat: add home page (#2)

---
+++ lib/home/view/home_page.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rocket_repository/rocket_repository.dart';
+import 'package:spacex_demo/home/home.dart';
+import 'package:spacex_demo/l10n/l10n.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => HomeCubit(
+        rocketRepository: context.read<RocketRepository>(),
+      )..fetchAllRockets(),
+      child: HomeView(),
+    );
+  }
+}
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+
+    return Scaffold(
+      appBar: AppBar(title: Text(l10n.homeAppBarTitle)),
+      body: const Center(
+        child: _Content(),
+      ),
+    );
+  }
+}
+
+class _Content extends StatelessWidget {
+  const _Content({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    final status = context.select((HomeCubit cubit) => cubit.state.status);
+
+    switch (status) {
+      case HomeStatus.initial:
+        return const SizedBox(
+          key: Key('homeView_initial_sizedBox'),
+        );
+      case HomeStatus.loading:
+        return const Center(
+          key: Key('homeView_loading_indicator'),
+          child: CircularProgressIndicator.adaptive(),
+        );
+      case HomeStatus.failure:
+        return Center(
+          key: const Key('homeView_failure_text'),
+          child: Text(l10n.homeRocketFetchErrorMessage),
+        );
+      case HomeStatus.success:
+        return const _RocketList(
+          key: Key('homeView_success_rocketList'),
+        );
+    }
+  }
+}
+
+class _RocketList extends StatelessWidget {
+  const _RocketList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final rockets = context.select((HomeCubit cubit) => cubit.state.rockets!);
+
+    return ListView(
+      children: [
+        for (final rocket in rockets) ...[
+          ListTile(
+            isThreeLine: true,
+            title: Text(rocket.name),
+            subtitle: Text(
+              rocket.description,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            trailing: const Icon(Icons.chevron_right_sharp),
+          ),
+          const Divider(),
+        ],
+      ],
+    );
+  }
+}
--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -1,26 +1,36 @@
-// Copyright (c) 2021, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:rocket_repository/rocket_repository.dart';
 import 'package:spacex_demo/l10n/l10n.dart';
 
+class MockRocketRepository extends Mock implements RocketRepository {}
+
 extension PumpApp on WidgetTester {
-  Future<void> pumpApp(Widget widget) {
+  Future<void> pumpApp(
+    Widget widget, {
+    RocketRepository? rocketRepository,
+  }) {
     return pumpWidget(
-      MaterialApp(
-        localizationsDelegates: [
-          AppLocalizations.delegate,
-          GlobalMaterialLocalizations.delegate,
+      MultiRepositoryProvider(
+        providers: [
+          RepositoryProvider.value(
+            value: rocketRepository ?? MockRocketRepository(),
+          )
         ],
-        supportedLocales: AppLocalizations.supportedLocales,
-        home: widget,
+        child: MaterialApp(
+          localizationsDelegates: [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+          ],
+          supportedLocales: AppLocalizations.supportedLocales,
+          home: Scaffold(
+            body: widget,
+          ),
+        ),
       ),
     );
   }
